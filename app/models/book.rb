c701f82b87bdc895f64db7d85167e92a8f0533ed
natsuokawai <kawai.natsuo@gmail.com>
2021-02-18 22:44:50 +0900

    Add anotation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
-# typed: strict
+# typed: true
 class Book < ApplicationRecord
+  extend T::Sig
+
+  sig { returns(String) }
+  def title_with_publish_date
+    "#{publish_date.strftime('%Y/%-m/%-d')} #{title}"
+  end
 end
